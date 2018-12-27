eee773086f5776f6813dde6cde98f4d8778091ba
Uzondu Enudeme <willpoint@gmail.com>
2018-12-27 01:19:04 +0100

    add dynamic functions to make store robust
    This involves creating an array to hold the type of state data
    The data is that array would be used to create the initial state,
    and to generate all getter methods at runtime

--- store/index.js
+++ store/index.js
@@ -3,26 +3,41 @@ import createWebSocketPlugin from '@/utils/socket.js'
 const url = process.env.socketUrl
 const ws = new WebSocket(url)
 
+const dataTypes = [
+  'version', 'branches', 'files'
+]
+
 export const state = () => ({
-  version: 'Unknown version',
   data: '',
+  ...genState(dataTypes)
 })
 
 export const plugins = [createWebSocketPlugin(ws)]
 
 export const getters = {
-  version(state) { return state.version },
-  output(state) { return state.data }
+  output(state) { return state.data },
+  ...genGetters(dataTypes)
 }
 
 export const mutations = {
   SET_DATA(state, {type, data}) { 
-    if (type === 'version') {
-      state.version = data
-    } else {
-      state.data = data 
-    }
+    state[type] = data 
   },
   GET_DATA(state, payload) {}
 }
 
+function genState(types) {
+  const states = {}
+  for (let i = 0; i <types.length; i++) {
+    states[types[i]] = ''
+  }
+  return states
+}
+
+function genGetters(types) {
+  const getters = {}
+  for (let i = 0; i < types.length; i++) {
+    getters[types[i]] = (state) => state[types[i]]
+  }
+  return getters
+}
