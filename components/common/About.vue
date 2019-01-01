cad3a277c7e67a6ac1b1598f719cad305e8f3f2b
Uzondu Enudeme <willpoint@gmail.com>
2019-01-01 19:56:08 +0100

    Update: provide good prose for the about modal
    The about menu item on the navigation bar should show a good summary
    of the program - describing in brief the purpose of the program
    stating also that it is free to use, with the email of the designer for
    comments and suggestions on making the program better

--- components/common/About.vue
+++ components/common/About.vue
@@ -1,22 +1,23 @@
 <template>
   <div class="modal-card" style="width: auto">
     <header class="modal-card-head">
-      <p class="modal-card-title is-6">About GitPlace</p>
+      <p class="modal-card-title has-text-gold is-6">GitPlace</p>
     </header>
     <section class="modal-card-body">
       <div class="content">
-        <p>
-          GitPlace is a tool to understand the history of a Git 
-        </p>
-        <p>
-        repository in a read-only, simple interface.
-        </p>
-        Send suggestions to the developer at - willpoint@gmail.com
-          
+        <div class="has-text-dark is-6">
+          GitPlace is a tool to understand the history of a Git <br/>
+          repository in a read-only, simple interface.
+          <hr />
+          Send suggestions to the developer at 
+          <a 
+            href="mailto:willpoint@gmail.com">
+            willpoint@gmail.com
+          </a>
+        </div>         
       </div>
-      <p class="title is-6">GitPlace is running {{ version }}</p>
+      <p class="title tag is-light is-6">GitPlace is free to use</p>
     </section>
-    
   </div>
 </template>
 
